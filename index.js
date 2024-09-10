86061475f66a4d0bfa2d8cf2ca76ba5aab2a85d0
Beautifulmoon211 <sweemmizuki211@gmail.com>
2024-09-10 05:44:26 +0200

    fix: restructure codes Co-authored-by: highambition211 <highambition2111@gmail.com>

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const redfinPropertyScraper = require('./redfinPropertyScraper')
 
+const API_KEY = process.env.API_KEY // <--- Enter your API key here
 const REDFIN_PAGE_URL = 'https://www.redfin.com/city/12839/DC/Washington-DC';
-const API_URL = 'https://api.scraperapi.com';
-const API_KEY = '5895fb6ca5cef34e820cca3250e1ef0b' // <--- Enter your API key here
 
-const webScraper = async () => {
-    console.log('Fetching data with ScraperAPI...');
-
-    const queryParams = new URLSearchParams({
-        api_key: API_KEY,
-        url: REDFIN_PAGE_URL,
-        country_code: 'us'
-    });
-
-    try {
-        const response = await axios.get(`${API_URL}?${queryParams.toString()}`);
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const propertyList = [];
-        console.log('Extracting information from the HTML...');
-
-        $(".HomeCardContainer").each((_, el) => {
-            const price = $(el).find('.bp-Homecard__Price--value.span') > 0 ? 
-                $(el).find('.bp-Homecard__Price--value.span').text() : 
-                $(el).find('.bp-Homecard__Price--value').text();
-            const beds = $(el).find('.bp-Homecard__Stats--beds.text-nowrap').text();
-            const bath = $(el).find('.bp-Homecard__Stats--baths.text-nowrap').text();
-            const space = $(el).find('.bp-Homecard__LockedStat--value').text();
-            const address = $(el).find('bp-Homecard__Address--address') > 0 ? 
-                $(el).find('.bp-Homecard__Address--address').text() : 
-                $(el).find('.bp-Homecard__Address').text();
-            const link = $(el).find('.link-and-anchor.visuallyHidden').attr('href');
-            const linkText = $(el).find('.link-and-anchor.visuallyHidden').text();
-            const contact = $(el).find('.RentalCTAContact__button--phone .ButtonLabel').text();
-            
-            if (!price) {
-                return
-            }
-
-            propertyList.push({
-                price,
-                address: address ?? linkText,
-                beds,
-                bath,
-                space,
-                link: link ? `https://www.redfin.com${link}` : null,
-                contact
-            });
-        });
-        console.log('JSON result:', propertyList);
-    } catch (error) {
-        console.log(error)
-    }
-};
-
-void webScraper();
+redfinPropertyScraper(API_KEY, REDFIN_PAGE_URL)
